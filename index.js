c741f77874e482867a4ca7abad4da5e8578c2cb0
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-16 11:27:04 +0500

    modified input

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const expensesInputNode = document.querySelector('[data-find="expensesInput"]');
 const buttonNode = document.getElementById("addedButton");
 
 buttonNode.addEventListener("click", function () {
-  const expense = expensesInputNode.value;
+  const expense = parseInt(expensesInputNode.value);
   expenses.push(expense);
 
   console.log(expenses);
