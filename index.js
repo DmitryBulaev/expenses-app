7d142b16f6ec43d5ab2547fed11d80062361d088
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-16 11:36:42 +0500

    input done

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const expensesInputNode = document.querySelector('[data-find="expensesInput"]');
 const buttonNode = document.getElementById("addedButton");
 
 buttonNode.addEventListener("click", function () {
+  if (!expensesInputNode.value) {
+    return;
+  }
   const expense = parseInt(expensesInputNode.value);
   expenses.push(expense);
+
+  expensesInputNode.value = "";
 
   console.log(expenses);
 });
