e430bfd443df95929c9b025eed254173d473544a
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-16 11:21:23 +0500

    added input and button

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-let expenses = [];
+const expenses = [];
+
+const expensesInputNode = document.querySelector('[data-find="expensesInput"]');
+const buttonNode = document.getElementById("addedButton");
+
+buttonNode.addEventListener("click", function () {
+  const expense = expensesInputNode.value;
+  expenses.push(expense);
+
+  console.log(expenses);
+});
