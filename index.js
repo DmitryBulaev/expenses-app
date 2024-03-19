b782d6448374ae175908617d27815d2a7b3880f6
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-19 16:29:42 +0500

    added draft for expenses history

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const expenses = [];
 
 const expensesInputNode = document.querySelector('[data-find="expensesInput"]');
 const buttonNode = document.getElementById("addedButton");
+const expensesHistoryNode = document.querySelector(
+  '[data-find="expensesHistory"]'
+);
 
 buttonNode.addEventListener("click", function () {
   if (!expensesInputNode.value) {
     return;
   }
   const expense = parseInt(expensesInputNode.value);
+  expensesInputNode.value = "";
   expenses.push(expense);
 
-  expensesInputNode.value = "";
+  let expensesListHTML = "";
+  expenses.forEach((element) => {
+    expensesListHTML += `<li>${element}</li>`;
+  });
 
-  console.log(expenses);
+  expensesHistoryNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
 });
