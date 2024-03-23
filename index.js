87655c7eb7400135be7e7f7871a6a3878a278daf
Dmitry Bulaev <bulaevd74@mail.ru>
2024-03-23 10:46:28 +0500

    added expenses limit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const expenses = [];
 
+const LIMIT = 10000;
+
 const expensesInputNode = document.querySelector('[data-find="expensesInput"]');
 const buttonNode = document.getElementById("addedButton");
 const expensesHistoryNode = document.querySelector(
   '[data-find="expensesHistory"]'
 );
 const sumNode = document.querySelector('[data-find="total"]');
+const limitNode = document.querySelector('[data-find="limit"]');
+
+limitNode.innerText = LIMIT;
 
 buttonNode.addEventListener("click", function () {
   if (!expensesInputNode.value) {
